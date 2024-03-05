a36d2cb31dbcea4577a45f074a2f12d8ad264540
rubences <91731294+rubences@users.noreply.github.com>
2024-03-05 14:57:59 +0000

    cambios

--- factorial.py
+++ factorial.py
@@ -1,22 +1,39 @@
-#generame una funcion recursiva del factorial de 5
-def factorial_recursivo(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial_recursivo(n-1)
+class Factorial:
+    def __init__(self, n):
+        self.n = n
 
+    def __str__(self):
+        return "Factorial de " + str(self.n)
 
-#generame una funcion iterativa del factorial de 5
-def factorial_iterativo(n):
-    result = 1
-    for i in range(1, n+1):
-        result *= i
-    return result
+    #generame los getter y setter de la variable n
+    def get_n(self):
+        return self.n
+
+    def set_n(self, n):
+        self.n = n
+    
+    def factorial_iterativo(self):
+        result = 1
+        for i in range(1, self.n+1):
+            result *= i
+        return result
+
+    def factorial_recursivo(self):
+
+        if self.n == 0:
+            return 1
+        else:
+            return self.n * Factorial(self.n-1).factorial_recursivo()
 
 #generame un main para llamar a las dos funciones
 def main():
-    print(factorial_recursivo(5))
-    print(factorial_iterativo(5))
+    factorial = Factorial(0)  # Instantiate the Factorial class
+    n = int(input("Ingrese un numero para calcular el factorial: "))
+    factorial.set_n(n)
+    print(factorial)
+    print("Este es Factorial Recursivo " + str(factorial.factorial_recursivo()))
+    print("Este es Factorial Iterativo " + str(factorial.factorial_iterativo()))
+
 
 if __name__ == "__main__":
     main()
